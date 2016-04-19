8354000094c12319640685e01a7e0fe82770f35b
Guille Smallwood <loyswd@gmail.com>
2016-04-19 07:32:08 -0500

    Added files via upload

---
+++ new.sql
@@ -0,0 +1,10 @@
+CREATE database reg_usuario
+use  reg_usuario
+create  TABLE usuario(
+Nombre varchar(15),
+Apellido varchar(15),
+Correo electronico varchar(40),
+celular BIGINT(20),
+sexo varchar(1),
+contraseña varchar(12),
+repita contraseña(12));
