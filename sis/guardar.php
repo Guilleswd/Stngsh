8e48b3c0de5a5740983bb6431205a47d46cabfec
Guille Smallwood <loyswd@gmail.com>
2016-05-03 21:50:52 -0500

    Cambios por revisar
    Se hicieron algunos cambios en el php,  y en la pagina del cliente.

---
+++ sis/guardar.php
@@ -0,0 +1,13 @@
+
+<?php 
+	$nom=$_POST["nom"];
+	$ape=$_POST["ape"];
+	$user=$_POST["user"];
+	$corr=$_POST["corr"];
+	$contr=$_POST["contr"];
+	$repcontr=$_POST["repcontr"];
+	$db=mysql_connect("localhost","root","");
+	mysql_select_db("stngh",$db);
+	mysql_query("insert into cliente values('$nom','$ape','$user','$corr','$contr','$repcontr')");
+	
+?>
