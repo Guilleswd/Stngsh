d175691954ab89243921291f6e67127d0123c779
Guille Smallwood <loyswd@gmail.com>
2016-05-11 08:01:04 -0500

    Add files via upload

---
+++ sis/buscar.php
@@ -0,0 +1,40 @@
+<?php 
+
+	$nombre=$_POST["nom"];
+	$db=mysql_connect("localhost","root","");
+	mysql_select_db("stngsh",$db);
+	$sql="select *from clientess where nombre='$nombre'";
+
+	$result = mysql_query($sql);
+?>
+<table border="2">
+<tr>
+		<td>Nombre</td>
+		<td>Apellido</td>					      
+		<td>User</td>					      
+		<td>Correo</td>	
+		<td>Contraseña</td>
+		<td>Repita Contraseña</td>	
+	</tr>
+<?php
+while ($row= mysql_fetch_row($result)) {
+	?>
+		
+	<tr>
+		<td><?= $row[0] ?></td>
+		<td><?= $row[1] ?></td>					      
+		<td><?= $row[2] ?></td>					      
+		<td><?= $row[3] ?></td>	
+		<td><?= $row[4] ?></td>	
+		<td><?= $row[5] ?></td>	
+	</tr>
+
+<?php 
+ } 
+?>
+</table>
+
+
+<h1> guardado correctamente</h1>
+
+<a href="index.html"> REGRESAR</a>
